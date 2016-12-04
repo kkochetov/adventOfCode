1890eeec6ab8ca610d4c05d0f0174627b59607f1
konstantin kochetov <kochetov@gmail.com>
2016-12-04 22:44:51 +0500

    day4

--- main.cpp
+++ main.cpp
@@ -2,17 +2,38 @@
 #include <c++/4.8.3/cstring>
 #include <fstream>
 #include <c++/4.8.3/vector>
+#include <regex>
+#include <c++/4.8.3/map>
+
 
 using namespace std;
 
 class Room{
 public:
     Room(std::string input){
+        std::string part = "";
+        std::string numb = "";
+
+        for (int i=0; i < input.size(); i++){
+            if (input[i] == '['){
+                this->checksum = input.substr(i+1,5);
+                break;
+            }
+            if (isNumber(input[i])){
+                numb += input[i];
+                continue;
+            }
+            if (input[i] != '-'){
+                part += input[i];
+            }
+        }
+            this->roomCode =  part;
+            this->number =  std::stoi(numb);
 
     }
 
     bool isReal(){
-        return checksum == process(this->roomCode);
+        return checksum == process();
     }
 
     int getNumber(){
@@ -24,8 +45,34 @@ private:
     std::string roomCode;
     int number;
 
-    std::string process(std::string ){
+    std::string process(){
+        map<char, int> alphabet;
+        for (int i = 0; i < this->roomCode.size(); i++){
+            if (roomCode[i] != '-') alphabet[roomCode[i]]++;
+        }
+        std::vector<std::pair<char, int>> charArray;
+        for(auto it = alphabet.begin(); it != alphabet.end(); it++){
 
+                charArray.push_back(std::make_pair(it->first, it->second));
+        }
+        auto cmp = [](std::pair<char,int> const & a, std::pair<char,int> const & b)
+        {
+            return a.second != b.second?  a.second > b.second : a.first < b.first;
+        };
+
+        std::sort(charArray.begin(), charArray.end(),cmp);
+
+        std::string result = "";
+        for (int i=0; i < 5; i++){
+            result += charArray[i].first;
+        }
+        cout << this->roomCode << " ! " << this->checksum << " ! " << result << " ! " << endl;
+        return result;
+    }
+
+    bool isNumber(char c){
+        int x = c;
+        return  (x >= 48) && (x <= 57);
     }
 };
 
@@ -57,6 +104,7 @@ int main()
     }
 
     cout << count;
+
     return 0;
 }
 
